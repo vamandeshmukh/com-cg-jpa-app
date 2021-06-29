facb783923d3c6f93dc9c562f742eee92c52b46e
vamandeshmukh <vamandeshmukh@gmail.com>
2021-06-29 12:27:14 +0530

    Update App.java

--- src/main/java/com/cg/jpa/app/demo/App.java
+++ src/main/java/com/cg/jpa/app/demo/App.java
@@ -18,13 +18,20 @@ public class App {
 		Session session = factory.openSession();
 		Transaction transaction = session.beginTransaction();
 
-		Department dep1 = new Department("HR", "Hyderabad");
-		session.save(dep1);
+//		Department dep1 = new Department("HR", "Hyderabad");
+//		session.save(dep1);
+//		Employee emp1 = new Employee("Sonu", 10.5, dep1);
+//		session.save(emp1);
+//		System.out.println(session.get(Employee.class, emp1.getEid()).toString());
 
-		Employee emp1 = new Employee("Sonu", 10.5, dep1);
-		session.save(emp1);
-
-		System.out.println(session.get(Employee.class, emp1.getEid()).toString());
+		Department dep2 = new Department("Sales", "Bengaluru");
+		session.save(dep2);
+		Employee emp2 = new Employee("Monu", 20.25, dep2);
+		session.save(emp2);
+		System.out.println(session.get(Employee.class, emp2.getEid()).toString());
+		Employee emp3 = new Employee("Monu", 20.25, dep2);
+		session.save(emp3);
+		System.out.println(session.get(Employee.class, emp3.getEid()).toString());
 
 		transaction.commit();
 		session.close();
